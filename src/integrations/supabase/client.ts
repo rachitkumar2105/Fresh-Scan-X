4ac4c83ea3d2a263999bbd05f77ebb7aad7841f2
Rachit Kumar Singh <147301141+R-ac-hi-t@users.noreply.github.com>
2026-01-23 12:26:56 +0530

    Update Supabase credentials

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://nbspisenvdjnzotimyvn.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpeWN0YXF1cGxuYXBkeXpiZ2ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc0NTAzNDQsImV4cCI6MjA4MzAyNjM0NH0.SGeERXZD_P0U6_BMaOrjjCTa5jr_NfrKbXFtzhJ5TZk";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_-EFetm0DZ42o6hRElB0pNA_2nlziwJ5";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
